ee49869393dcfabde8bd5d4c5f18cd3bf803ffe6
Megan Leich <51799170+maleich@users.noreply.github.com>
2022-04-10 15:08:45 -0700

    update starter code

--- src/my_code.py
+++ src/my_code.py
@@ -5,8 +5,13 @@
 #
 #############################
 def main():
-
-
+    # get inputs. Be sure to answer Yes or No (capitalized) when you run your program
+    
+    # check answers. Use the responses below where appropriate.
+    # Do NOT change the text or the checks will fail!
+    
+    print("Sleep in? Yes!")
+    print("Sleep in? No!")
 
 
 if __name__ == "__main__":
